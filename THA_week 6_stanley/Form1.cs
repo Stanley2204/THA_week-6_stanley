f757af302fe8bb03e41e0511ab8fb8ef23ae69bb
stanley <stanley@DESKTOP-Q11B730>
2023-04-08 15:43:00 +0700

    Add project files.

---
+++ THA_week 6_stanley/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THA_week_6_stanley
+{
+    public partial class Form1 : Form
+    {
+        Form2 nextform = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBox1.Text) < 4)
+            {
+                MessageBox.Show("INPUTTT WOIII");
+            }
+            else
+            {
+                Form2.sizing = Convert.ToInt32(textBox1.Text);
+                nextform.Show();
+            }
+
+        }
+    }
+}
